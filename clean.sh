f9ea156049ad507344e54bad06390ab3c3cdeb73
ZIXUAN ZHU <162306639+zhu30844@users.noreply.github.com>
2026-04-15 14:42:09 +0800

    Fix formatting issue in clean.sh

--- clean.sh
+++ clean.sh
@@ -5,6 +5,6 @@ EXTERNAL_REL_BUILDROOT=../base_external
 set -e
 cd `dirname $0`
 
-make -C buildroot distclean BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} 
+make -C buildroot distclean BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
 
 
